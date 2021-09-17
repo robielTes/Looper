4336d48e708922527e7e8210f4e7df2ea7682f64
robielcpnv <robiel.tesfazghi@cpnv.ch>
2021-09-17 11:04:27 +0200

    redirect

--- views/templates/template.php
+++ views/templates/template.php
@@ -6,10 +6,11 @@
     <link rel="stylesheet" href="views/css/tailwind.css">
 </head>
 <body>
-<div class="px-12 sm:px-24 md:px-40 header <?= $color ?>">
+<a href="\"><div class="px-12 sm:px-24 md:px-40 header <?= $color ?>">
     <img src="views/assets/logo.png"/>
     <p class="title"><?= $title ?></p>
 </div>
+</a>
 <div class="px-12 sm:px-24 md:px-40">
     <?= $content ?>
 </div>
