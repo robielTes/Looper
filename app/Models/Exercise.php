c77a14a4d582283c0c284d94e47032476b2a0367
TESFAZGHI Robiel <Robiel.TESFAZGHI@cpnv.ch>
2021-11-08 15:33:06 +0100

    Add namespace for models

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -1,7 +1,9 @@
 <?php
 
+namespace App\Models;
 use App\Models;
 use filu\maw_orm\Model;
+
 
 class Exercise extends Model
 {
--- app/Models/Line.php
+++ app/Models/Line.php
@@ -1,6 +1,6 @@
 
 <?php
-
+namespace App\Models;
 use App\Models;
 use filu\maw_orm\Model;
 
