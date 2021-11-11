58ec9bb7a0b827ed4daae057c8699021eba7975f
robielcpnv <robiel.tesfazghi@cpnv.ch>
2021-11-11 08:41:29 +0100

    update documentation

---
+++ looper.sql
@@ -0,0 +1,138 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema looper
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `looper`;
+-- -----------------------------------------------------
+-- Schema looper
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `looper` DEFAULT CHARACTER SET utf8mb4 ;
+USE `looper` ;
+
+-- -----------------------------------------------------
+-- Table `looper`.`states`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `looper`.`states` (
+                                                 `id` INT NOT NULL AUTO_INCREMENT,
+                                                 `name` VARCHAR(10) NOT NULL,
+    PRIMARY KEY (`id`))
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `looper`.`exercises`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `looper`.`exercises` (
+                                                    `id` INT NOT NULL AUTO_INCREMENT,
+                                                    `title` LONGTEXT NULL,
+                                                    `states_id` INT NOT NULL DEFAULT 1,
+                                                    PRIMARY KEY (`id`),
+    INDEX `fk_exercises_states1_idx` (`states_id` ASC) VISIBLE,
+    CONSTRAINT `fk_exercises_states1`
+    FOREIGN KEY (`states_id`)
+    REFERENCES `looper`.`states` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `looper`.`lines`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `looper`.`lines` (
+                                                `id` INT NOT NULL AUTO_INCREMENT,
+                                                `kind` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`id`))
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `looper`.`fields`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `looper`.`fields` (
+                                                 `id` INT NOT NULL AUTO_INCREMENT,
+                                                 `label` LONGTEXT NULL,
+                                                 `lines_id` INT NOT NULL DEFAULT 1,
+                                                 `exercises_id` INT NOT NULL,
+                                                 PRIMARY KEY (`id`),
+    INDEX `fk_fields_lines_idx` (`lines_id` ASC) VISIBLE,
+    INDEX `fk_fields_exercises1_idx` (`exercises_id` ASC) VISIBLE,
+    CONSTRAINT `fk_fields_lines`
+    FOREIGN KEY (`lines_id`)
+    REFERENCES `looper`.`lines` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+    CONSTRAINT `fk_fields_exercises1`
+    FOREIGN KEY (`exercises_id`)
+    REFERENCES `looper`.`exercises` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `looper`.`answers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `looper`.`answers` (
+                                                  `id` INT NOT NULL AUTO_INCREMENT,
+                                                  `take` DATETIME NOT NULL DEFAULT now(),
+    `answer` LONGTEXT NULL,
+    `fields_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_answer_fields1_idx` (`fields_id` ASC) VISIBLE,
+    CONSTRAINT `fk_answer_fields1`
+    FOREIGN KEY (`fields_id`)
+    REFERENCES `looper`.`fields` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+    ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- Lines
+
+INSERT INTO `looper`.`lines` (`kind`) VALUES ('Single line text');
+INSERT INTO `looper`.`lines` (`kind`) VALUES ('List of single lines');
+INSERT INTO `looper`.`lines` (`kind`) VALUES ('Multi-line text');
+
+-- State
+
+INSERT INTO `looper`.`states` (`name`) VALUES ('Building');
+INSERT INTO `looper`.`states` (`name`) VALUES ('Answering');
+INSERT INTO `looper`.`states` (`name`) VALUES ('Closed');
+
+
+-- Exercises
+
+INSERT INTO `looper`.`exercises` (`title`, `states_id`) VALUES ('Manger ou bouger ?', '1');
+INSERT INTO `looper`.`exercises` (`title`, `states_id`) VALUES ('Le sport pour vous', '2');
+INSERT INTO `looper`.`exercises` (`title`, `states_id`) VALUES ('Une enquête unique au sujet de moi', '3');
+INSERT INTO `looper`.`exercises` (`title`, `states_id`) VALUES ('Pour combien ?', '2');
+
+-- Fields
+
+INSERT INTO `looper`.`fields` (`label`, `lines_id`, `exercises_id`) VALUES ('Cola ou fanta', '2' ,'1');
+INSERT INTO `looper`.`fields` (`label`, `lines_id`, `exercises_id`) VALUES ('Corde à sauter ou trampoline', '3' ,'1');
+INSERT INTO `looper`.`fields` (`label`, `lines_id`, `exercises_id`) VALUES ('WWF ou WWE', '2' ,'1');
+INSERT INTO `looper`.`fields` (`label`, `lines_id`, `exercises_id`) VALUES ('Intéressant ou pas', '1','2');
+INSERT INTO `looper`.`fields` (`label`, `lines_id`, `exercises_id`) VALUES ('Combien de fois par semaine', '2' ,'2');
+INSERT INTO `looper`.`fields` (`label`, `lines_id`, `exercises_id`) VALUES ('Je suis beau ?', '3' ,'3');
+INSERT INTO `looper`.`fields` (`label`, `lines_id`, `exercises_id`) VALUES ('Pour combien, tu sautes', '1' ,'4');
+INSERT INTO `looper`.`fields` (`label`, `lines_id`, `exercises_id`) VALUES ('Pour combien, tu frappes', '1' ,'4');
+
+
+-- Answer
+
+INSERT INTO `looper`.`answers` (`answer`, `fields_id`) VALUES ('Oui', '1');
+INSERT INTO `looper`.`answers` (`answer`, `fields_id`) VALUES ('Je dirais plutôt non', '2');
+INSERT INTO `looper`.`answers` (`answer`, `fields_id`) VALUES ('A voir', '1');
+INSERT INTO `looper`.`answers` (`answer`, `fields_id`) VALUES ('Pas possible', '4');
+INSERT INTO `looper`.`answers` (`answer`, `fields_id`) VALUES ('Exactement', '2');
